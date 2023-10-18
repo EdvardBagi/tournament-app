5d071123b4fd985edbc110af037cb6b1e5d26102
EdvardBagi <bagedvardegyetem@gmail.com>
2023-10-18 18:15:09 +0200

    Project upload.

---
+++ resources/js/handler.js
@@ -0,0 +1,135 @@
+$(document).ready(function () {
+    $.ajaxSetup({
+        headers:
+            {'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')}
+    });
+    $('#submitTournament').on('click', function (e) {
+        e.preventDefault();
+        console.log('Tried');
+        let name = $('#nameT').val();
+        let year = $('#year').val();
+        $.ajax({
+            type: 'POST',
+            url: $('meta[name="TS"]').attr('content'),
+            data: {'nameT': name, 'year': year},
+            success: function () {
+                $('#tournamentForm')[0].reset();
+                $('#tournamentTable').load(document.URL + ' #tournamentTable');
+                $('#roundForm').load(document.URL + ' #roundForm');
+            },
+            error: function () {
+                alert('Tournament already exists.');
+            }
+        });
+    });
+});
+$(document).on('click', '#submitRound', function (e) {
+    e.preventDefault();
+    let name = $('#nameR').val();
+    let both = $('#both option:selected').text();
+    let arr = both.split(",");
+    let name2 = arr[0];
+    let year = arr[1];
+    $.ajax({
+        type: 'POST',
+        url: $('meta[name="RS"]').attr('content'),
+        data: {'nameR': name, 'nameTournament': name2, 'year': year},
+        success: function () {
+            $('#roundForm').load(document.URL + ' #roundForm');
+            $('#roundTable').load(document.URL + ' #roundTable');
+            $('#contestantForm').load(document.URL + ' #contestantForm');
+        },
+        error: function () {
+            alert('Round for this tournament already exists.');
+        }
+    });
+});
+$(document).on('click', '#submitContestant', function (e) {
+    e.preventDefault();
+    let name = $('#user option:selected').text();
+    let round = $('#round option:selected').text();
+    let list = name.split(",");
+    let arr = round.split(",");
+    $.ajax({
+        type: 'POST',
+        url: $('meta[name="CS"]').attr('content'),
+        data: {'id': list[0],'round': arr[0],'tournament_name':arr[1],'tournament_year':arr[2]},
+        success: function () {
+            $('#contestantForm').load(document.URL + ' #contestantForm');
+            $('#contestantTable').load(document.URL + ' #contestantTable');
+        },
+        error: function () {
+            alert('Contestant to this round and tournament already exists.');
+        }
+    });
+});
+
+//DELETES
+$(document).on('click', '.deleteTournament', function (e) {
+    e.preventDefault();
+    let row = $(this).closest('tr');
+    let nameT = row.find('td:nth-child(1)').text();
+    let yearT = row.find('td:nth-child(2)').text();
+    console.log(nameT, yearT);
+    $.ajax({
+        type: 'POST',
+        url: $('meta[name="TD"]').attr('content'),
+        data: {'name': nameT, 'year': yearT},
+        success: function () {
+            $('#tournamentTable').load(document.URL + ' #tournamentTable');
+            $('#roundForm').load(document.URL + ' #roundForm');
+            $('#roundTable').load(document.URL + ' #roundTable');
+            $('#contestantTable').load(document.URL + ' #contestantTable');
+            $('#contestantForm').load(document.URL + ' #contestantForm');
+        }
+    });
+});
+$(document).on('click', '.deleteRound', function (e) {
+    e.preventDefault();
+    let row = $(this).closest('tr');
+    let round = row.find('td:nth-child(1)').text();
+    let TName = row.find('td:nth-child(2)').text();
+    let TYear = row.find('td:nth-child(3)').text();
+    $.ajax({
+        type: 'POST',
+        url: $('meta[name="RD"]').attr('content'),
+        data: {'round': round, 'TName': TName, 'TYear': TYear},
+        success: function () {
+            $('#roundTable').load(document.URL + ' #roundTable');
+            $('#contestantTable').load(document.URL + ' #contestantTable');
+            $('#contestantForm').load(document.URL + ' #contestantForm');
+        }
+    });
+});
+$(document).on('click', '.deleteContestant', function (e) {
+    e.preventDefault();
+    let row = $(this).closest('tr');
+    let id = row.find('td:nth-child(1)').text();
+    let round = row.find('td:nth-child(3)').text();
+    let tournament_name = row.find('td:nth-child(4)').text();
+    let tournament_year = row.find('td:nth-child(5)').text();
+    $.ajax({
+        type: 'POST',
+        url: $('meta[name="CD"]').attr('content'),
+        data: {'id': id, 'round': round,'tournament_name':tournament_name,'tournament_year':tournament_year},
+        success: function () {
+            $('#contestantTable').load(document.URL + ' #contestantTable');
+        }
+    });
+});
+$(document).on('click', '.deleteUser', function (e) {
+    e.preventDefault();
+    let row = $(this).closest('tr');
+    let name = row.find('td:nth-child(1)').text();
+    let email = row.find('td:nth-child(2)').text();
+    $.ajax({
+        type: 'POST',
+        url: $('meta[name="UD"]').attr('content'),
+        data: {'username': name, 'email': email},
+        success: function () {
+            $('#userTable').load(document.URL + ' #userTable');
+            $('#contestantTable').load(document.URL + ' #contestantTable');
+            $('#contestantForm').load(document.URL + ' #contestantForm');
+        }
+    });
+});
--- resources/views/actions.blade.php
+++ resources/views/actions.blade.php
@@ -4,150 +4,20 @@
     <meta charset="UTF-8">
     <meta name=”viewport” content=”width=device-width, initial-scale=1.0″>
     <meta name="_token" content="{{ csrf_token() }}"/>
+    <meta name="TS" content="{{ url("insertTournament") }}"/>
+    <meta name="RS" content="{{ url("insertRound") }}"/>
+    <meta name="CS" content="{{ url("insertContestant") }}"/>
+    <meta name="TD" content="{{ url("deleteTournament") }}"/>
+    <meta name="RD" content="{{ url("deleteRound") }}"/>
+    <meta name="CD" content="{{ url("deleteContestant") }}"/>
+    <meta name="UD" content="{{ url("deleteUser") }}"/>
+
     <title>Actions</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="../js/jquery-3.7.1.js"></script>
-    <script>
-        $(document).ready(function () {
-            $.ajaxSetup({
-                headers:
-                    {'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')}
-            });
-            $('#submitTournament').on('click', function (e) {
-                e.preventDefault();
-                let name = $('#nameT').val();
-                let year = $('#year').val();
-
-                $.ajax({
-                    type: 'POST',
-                    url: '{{ url("insertTournament") }}',
-                    data: {'nameT': name, 'year': year},
-                    success: function () {
-                        $('#tournamentForm')[0].reset();
-                        $('#tournamentTable').load(document.URL + ' #tournamentTable');
-                        $('#roundForm').load(document.URL + ' #roundForm');
-                    },
-                    error: function () {
-                        alert('Tournament already exists.');
-                    }
-                });
-            });
-        });
-        $(document).on('click', '#submitRound', function (e) {
-            e.preventDefault();
-            let name = $('#nameR').val();
-            let both = $('#both option:selected').text();
-            let arr = both.split(",");
-            let name2 = arr[0];
-            let year = arr[1];
-            $.ajax({
-                type: 'POST',
-                url: '{{ url("insertRound") }}',
-                data: {'nameR': name, 'nameTournament': name2, 'year': year},
-                success: function () {
-                    $('#roundForm').load(document.URL + ' #roundForm');
-                    $('#roundTable').load(document.URL + ' #roundTable');
-                    $('#contestantForm').load(document.URL + ' #contestantForm');
-                },
-                error: function () {
-                    alert('Round for this tournament already exists.');
-                }
-            });
-        });
-        $(document).on('click', '#submitContestant', function (e) {
-            e.preventDefault();
-            let name = $('#user option:selected').text();
-            let round = $('#round option:selected').text();
-            let list = name.split(",");
-            let arr = round.split(",");
-            $.ajax({
-                type: 'POST',
-                url: '{{ url("insertContestant") }}',
-                data: {'id': list[0],'round': arr[0],'tournament_name':arr[1],'tournament_year':arr[2]},
-                success: function () {
-                    $('#contestantForm').load(document.URL + ' #contestantForm');
-                    $('#contestantTable').load(document.URL + ' #contestantTable');
-                },
-                error: function () {
-                    alert('Contestant to this round and tournament already exists.');
-                }
-            });
-        });
-
-        //DELETES
-        $(document).on('click', '.deleteTournament', function (e) {
-            e.preventDefault();
-            let row = $(this).closest('tr');
-            let nameT = row.find('td:nth-child(1)').text();
-            let yearT = row.find('td:nth-child(2)').text();
-            console.log(nameT, yearT);
-            $.ajax({
-                type: 'POST',
-                url: '{{ url("deleteTournament") }}',
-                data: {'name': nameT, 'year': yearT},
-                success: function () {
-                    $('#tournamentTable').load(document.URL + ' #tournamentTable');
-                    $('#roundForm').load(document.URL + ' #roundForm');
-                    $('#roundTable').load(document.URL + ' #roundTable');
-                    $('#contestantTable').load(document.URL + ' #contestantTable');
-                    $('#contestantForm').load(document.URL + ' #contestantForm');
-                }
-            });
-        });
-        $(document).on('click', '.deleteRound', function (e) {
-            e.preventDefault();
-            let row = $(this).closest('tr');
-            let round = row.find('td:nth-child(1)').text();
-            let TName = row.find('td:nth-child(2)').text();
-            let TYear = row.find('td:nth-child(3)').text();
-            $.ajax({
-                type: 'POST',
-                url: '{{ url("deleteRound") }}',
-                data: {'round': round, 'TName': TName, 'TYear': TYear},
-                success: function () {
-                    $('#roundTable').load(document.URL + ' #roundTable');
-                    $('#contestantTable').load(document.URL + ' #contestantTable');
-                    $('#contestantForm').load(document.URL + ' #contestantForm');
-                }
-            });
-        });
-        $(document).on('click', '.deleteContestant', function (e) {
-            e.preventDefault();
-            let row = $(this).closest('tr');
-            let id = row.find('td:nth-child(1)').text();
-            let round = row.find('td:nth-child(3)').text();
-            let tournament_name = row.find('td:nth-child(4)').text();
-            let tournament_year = row.find('td:nth-child(5)').text();
-            $.ajax({
-                type: 'POST',
-                url: '{{ url("deleteContestant") }}',
-                data: {'id': id, 'round': round,'tournament_name':tournament_name,'tournament_year':tournament_year},
-                success: function () {
-                    $('#contestantTable').load(document.URL + ' #contestantTable');
-                }
-            });
-        });
-        $(document).on('click', '.deleteUser', function (e) {
-            e.preventDefault();
-            let row = $(this).closest('tr');
-            let name = row.find('td:nth-child(1)').text();
-            let email = row.find('td:nth-child(2)').text();
-            console.log(name, email);
-            $.ajax({
-                type: 'POST',
-                url: '{{ url("deleteUser") }}',
-                data: {'username': name, 'email': email},
-                success: function () {
-                    $('#userTable').load(document.URL + ' #userTable');
-                    $('#contestantTable').load(document.URL + ' #contestantTable');
-                    $('#contestantForm').load(document.URL + ' #contestantForm');
-                }
-            });
-        });
-    </script>
+    <script src="../js/handler.js"></script>
 </head>
 <body>
-<div hidden id="#hidden">{{ csrf_token() }}</div>
 @if(Auth::user() === null || Auth::user()->email != "admin@gmail.com")
     <script>window.location = "/"</script>
 @endif
